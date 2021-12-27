7099b0d26b393819eaa0d044e5d16cc71e0f217c
robtsai <zenrob@gmail.com>
2021-12-27 12:37:47 -0500

    fix

--- 21_dirac_dice_part1.py
+++ 21_dirac_dice_part1.py
@@ -1,4 +1,3 @@
-
 with open("input_files/problem21.txt", "r") as f:
     player1 = int(f.readline().split(":")[-1])
     player2 = int(f.readline().split(":")[-1])
@@ -7,6 +6,7 @@ with open("input_files/problem21.txt", "r") as f:
 BOARD_SLOTS = 10
 TARGET = 1000
 ROLLS_PER_TURN = 3
+
 
 class Die:
     # ie six sided rolls 1, 2, 3, 4, 5, 6, 1 ...
@@ -30,9 +30,9 @@ class Player:
         self.score = 0
 
     def move_on_board(self, steps):
-        """ we are on a ten sided board, so we take steps mod 10 and add to cur pos.
-        similar to our six sided die, we need to reset the position """
-        inc_steps = steps % 10
+        """we are on a ten sided board, so we take steps mod 10 and add to cur pos.
+        similar to our six sided die, we need to reset the position"""
+        inc_steps = steps % BOARD_SLOTS
         self.pos = (self.pos - 1 + inc_steps) % BOARD_SLOTS + 1
         self.score += self.pos
 
@@ -47,11 +47,11 @@ def roll(die, numrolls):
         next(die)
     return v
 
+
 def playgame():
     d = Die(100)
     p1 = Player("p1", player1)
     p2 = Player("p2", player2)
-
 
     turns = 0
     winner = None
@@ -65,7 +65,7 @@ def playgame():
 
         if p1.score >= TARGET:
             winner = p1
-            loser = p2 
+            loser = p2
             break
         moves = roll(d, ROLLS_PER_TURN)
         p2.move_on_board(moves)
@@ -74,7 +74,7 @@ def playgame():
 
         if p2.score >= TARGET:
             winner = p2
-            loser = p1  
+            loser = p1
             break
 
     print(f"The winner is {winner.name}")
@@ -82,6 +82,6 @@ def playgame():
     print(f"The number of turns is {turns}")
     print(f"The number of dice rolls is {turns * ROLLS_PER_TURN}")
     print(f"The answer to part 1 is {loser.score * turns * ROLLS_PER_TURN}")
-    
+
 
 playgame()
